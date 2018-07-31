1bd69caa1b1e07de5e9a86c8bfbd2efc016f1860
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-31 13:32:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,4 @@ console.error();
 
 console.log();
 
-  // it('calls console.warn()', done => {
-  //   const spy = expect.spyOn(console, 'warn').andCallThrough()
-
-  //   jsdom.env(html, [src], {
-  //     virtualConsole: jsdom.createVirtualConsole().sendTo(console)
-  //   }, (err, window) => {
-  //     expect(spy).toHaveBeenCalled('expected console.warn to have been called')
-  //     console.warn.restore()
-  //     done()
+console.warn();
